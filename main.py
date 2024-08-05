a8f1914ec1f90101e3eef2ad095b0acbb94b4a90
kllarena07 <baymax709@gmail.com>
2024-08-05 17:40:57 -0600

    feat: import config and configure

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 from parse_data import parse_data
+from configs import ModelConfigs
 
 dataset, vocab, max_len = parse_data()
+
+configs = ModelConfigs()
+configs.vocab = "".join(vocab)
+configs.max_text_length = max_len
+configs.save()
