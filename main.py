0c9bcdbb4d0f1a6fb7d8735850773f9eccde06b5
kllarena07 <baymax709@gmail.com>
2024-08-05 17:49:15 -0600

    chore: renamed main.py to train.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from parse_data import parse_data
-from configs import ModelConfigs
-
-dataset, vocab, max_len = parse_data()
-
-configs = ModelConfigs()
-configs.vocab = "".join(vocab)
-configs.max_text_length = max_len
-configs.save()
